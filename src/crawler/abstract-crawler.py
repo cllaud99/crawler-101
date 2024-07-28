027905e49d9b2e887261356116e9a7269bf383b3
ClaudioPontes <claudiohpontes@gmail.com>
2024-07-28 03:01:38 -0300

    trabalhando na implementação get_step

--- src/crawler/abstract-crawler.py
+++ src/crawler/abstract-crawler.py
@@ -1,18 +1,14 @@
 from abc import ABC, abstractmethod
-
 from loguru import logger
-
 from src.tools.redis import RedisClient
 from config_logger import configure_logger
 
 configure_logger()
 
-
 class AbstractCrawler(ABC):
 
     def __init__(self):
         self.redis = RedisClient.get()
-        pass
 
     @abstractmethod
     def execute_main(self):
@@ -27,12 +23,18 @@ class AbstractCrawler(ABC):
         pass
 
     def get_step(self, key):
-        return self.redis.get(key)
+        try:
+            steps = self.redis.get(key)
+            if steps is None:
+                logger.warning(f"Chave '{key}' não encontrada no Redis.")
+        except Exception as e:
+            logger.error(f"Não foi possível coletar os dados do Redis: {e}")
+            steps = None
+        return steps
 
     def save_data(self, data):
         try:
             self.mongo.save_dataframe(data)
-        except:
+            logger.info("Dados salvos com sucesso")
+        except Exception as e:
             logger.error(f"Não foi possível salvar os dados: {e}")
-            
-        pass
